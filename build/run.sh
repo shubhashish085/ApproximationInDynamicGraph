d103b3da1d2996e91c74c88343736cfc2f1c5131
Antu <shubhashishkar@gmail.com>
2025-11-19 09:48:05 -0800

    [add] dynamic graph and run.sh change added

--- build/run.sh
+++ build/run.sh
@@ -1,16 +1,12 @@
 #! /bin/bash
-./adg.out -d ./com-lj_stm_5d.ungraph.txt -o ./lj_thinkd_fd_1000_5d.txt -a 3 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_5d.ungraph.txt -o ./lj_triest_fd_1000_5d.txt -a 2 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_5d.ungraph.txt -o ./lj_mascot_fd_1000_5d.txt -a 1 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_5fd_l.ungraph.txt -o ./orkut_thinkd_fd_1000_5fd_l.txt -a 3 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_5fd_l.ungraph.txt -o ./orkut_triest_fd_1000_5fd_l.txt -a 2 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_5fd_l.ungraph.txt -o ./orkut_mascot_fd_1000_5fd_l.txt -a 1 -m 65536 -s 0.1
 
-./adg.out -d ./com-lj_stm_10d.ungraph.txt -o ./lj_thinkd_fd_1000_10d.txt -a 3 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_10d.ungraph.txt -o ./lj_triest_fd_1000_10d.txt -a 2 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_10d.ungraph.txt -o ./lj_mascot_fd_1000_10d.txt -a 1 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_10fd_l.ungraph.txt -o ./orkut_thinkd_fd_1000_10fd_l.txt -a 3 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_10fd_l.ungraph.txt -o ./orkut_triest_fd_1000_10fd_l.txt -a 2 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_10fd_l.ungraph.txt -o ./orkut_mascot_fd_1000_10fd_l.txt -a 1 -m 65536 -s 0.1
 
-./adg.out -d ./com-lj_stm_15d.ungraph.txt -o ./lj_thinkd_fd_1000_15d.txt -a 3 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_15d.ungraph.txt -o ./lj_triest_fd_1000_15d.txt -a 2 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_15d.ungraph.txt -o ./lj_mascot_fd_1000_15d.txt -a 1 -m 65536 -s 0.1
-
-./adg.out -d ./com-lj_stm_20d.ungraph.txt -o ./lj_thinkd_fd_1000_20d.txt -a 3 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_20d.ungraph.txt -o ./lj_triest_fd_1000_20d.txt -a 2 -m 65536 -s 0.1
-./adg.out -d ./com-lj_stm_20d.ungraph.txt -o ./lj_mascot_fd_1000_20d.txt -a 1 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_15fd_l.ungraph.txt -o ./orkut_thinkd_fd_1000_15fd_l.txt -a 3 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_15fd_l.ungraph.txt -o ./orkut_triest_fd_1000_15fd_l.txt -a 2 -m 65536 -s 0.1
+./adg.out -d ./com-orkut_stm_15fd_l.ungraph.txt -o ./orkut_mascot_fd_1000_15fd_l.txt -a 1 -m 65536 -s 0.1
