ce11efef25a2725b965cc2b95a7c1713579d94f9
antu <shubhashishkar@gmail.com>
2025-11-24 12:38:12 -0800

    [add] streaming graph reformatting added

--- ProcessDynamicGraph.cpp
+++ ProcessDynamicGraph.cpp
@@ -343,7 +343,7 @@ void create_streaming_graph_with_random_deletion(const std::string& file_path, c
 int main(int argc, char** argv){
 
     std::string input_graph_file = "./com-amazon_stm_5fd_l.ungraph.txt";
-    std::string output_graph_file = "com-amazon_stm_5fd_l_abacus.ungraph.txt";
+    std::string output_graph_file = "./com-amazon_stm_5fd_l_abacus.ungraph.txt";
 
     reformat_streaming_graph_from_file(input_graph_file, output_graph_file);
 }
